c0e895b2e233afc4689f6b3784c658decb85f8f3
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-16 09:15:17 -0700

    Feat: create initialize, loop? and total_distance methods for Ride class

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,25 @@
 class Ride
+    attr_reader :name,
+                :distance,
+                :terrain,
+                :loop
 
+    def initialize(info)
+        @name = info[:name]
+        @distance = info[:distance]
+        @terrain = info[:terrain]
+        @loop = info[:loop]
+    end
+
+    def loop?
+        @loop
+    end
+
+    def total_distance
+        if loop?
+            @distance
+        else
+            2 * @distance
+        end
+    end
 end
