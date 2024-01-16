0d974ba148903cf4a93084c32f8751a37bbbdf31
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-16 10:01:22 -0700

    Feat: create initialize and add_biker methods for BikeClub Class

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -1,3 +1,12 @@
 class BikeClub
+    attr_reader :name, :bikers
 
+    def initialize(name)
+        @name = name
+        @bikers = []
+    end
+
+    def add_biker(biker)
+        @bikers << biker
+    end
 end
