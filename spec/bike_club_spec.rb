8391b5ae82db8d23d4405436b9ac6289e8f6efb2
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-16 11:04:15 -0700

    Test: create tests for bikers_eligible method for BikeClub class

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe BikeClub do
         @bikeclub1 = BikeClub.new("Roosters")
         @biker = Biker.new("Kenny", 30)
         @biker2 = Biker.new("Athena", 15)
+        @biker3 = Biker.new("Mario", 100)
         @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
         @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
         @biker.learn_terrain!(:gravel)
@@ -18,6 +19,8 @@ RSpec.describe BikeClub do
         @biker.log_ride(@ride2, 61.6)
         @biker2.log_ride(@ride2, 65.0)
         @biker2.log_ride(@ride2, 60.0)
+        @biker3.log_ride(@ride1, 89.8)
+        @biker3.log_ride(@ride2, 60.4)
     end
     describe "#initialize" do
         it 'exists' do
@@ -50,11 +53,39 @@ RSpec.describe BikeClub do
     end
 
     describe "#best_time(ride)" do
-        it "can tell us which Biker has the best time for a given Ride." do
+        it "can tell which Biker has the best time for a given Ride." do
             @bikeclub1.add_biker(@biker)
             @bikeclub1.add_biker(@biker2)
+            @bikeclub1.add_biker(@biker3)
             
             expect(@bikeclub1.best_time(@ride2)).to eq(@biker2)
+            expect(@bikeclub1.best_time(@ride1)).to eq(@biker)
+
+            @biker3.learn_terrain!(:hills)
+            @biker3.log_ride(@ride1, 89.8)
+
+            expect(@bikeclub1.best_time(@ride1)).to eq(@biker3)
         end
+    end
+
+    describe "bikers_eligible(ride)" do
+        it "can tell which Bikers are eligible for a given Ride." do
+            @bikeclub1.add_biker(@biker)
+            @bikeclub1.add_biker(@biker2)
+
+            expect(@bikeclub1.bikers_eligible(@ride1)).to eq([@biker])
+            expect(@bikeclub1.bikers_eligible(@ride2)).to eq([@biker, @biker2])
+
+            @biker3.learn_terrain!(:hills)
+            @bikeclub1.add_biker(@biker3)
+
+            expect(@bikeclub1.bikers_eligible(@ride1)).to eq([@biker, @biker3])
+            expect(@bikeclub1.bikers_eligible(@ride2)).to eq([@biker, @biker2])
+
+            @biker3.learn_terrain!(:gravel)
+
+            expect(@bikeclub1.bikers_eligible(@ride2)).to eq([@biker, @biker2, @biker3])
+        end
+
     end
 end
