0833355c5e73b0c3be4bacbab4bc480044aa560b
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-16 10:55:12 -0700

    Feat: create eligible_distance? and acceptable_terrain? methods, and refactor log_ride method for Biker Class

--- lib/biker.rb
+++ lib/biker.rb
@@ -15,13 +15,25 @@ class Biker
         @acceptable_terrain << terrain
     end
 
+    def eligible_ride?(ride)
+        eligible_distance?(ride) && acceptable_terrain?(ride)
+    end
+
+    def eligible_distance?(ride)
+        ride.total_distance <= @max_distance
+    end
+
+    def acceptable_terrain?(ride)
+        @acceptable_terrain.include?(ride.terrain)
+    end
+
     def log_ride(ride, time)
-        if ride.total_distance > @max_distance
-            "Biker #{@name} can't bike this distance"
-        elsif !@acceptable_terrain.include?(ride.terrain)
-            "Biker #{@name} can't bike this terrain"
-        else        
+        if eligible_ride?(ride)
             @rides[ride] << time 
+        elsif !eligible_distance?(ride)
+            "Biker #{@name} can't bike this distance"
+        elsif !acceptable_terrain?(ride)
+            "Biker #{@name} can't bike this terrain"
         end
     end
 
